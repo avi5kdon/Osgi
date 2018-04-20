d9528b75c95e4c9584fd5f9c87e7a9134b2cd1b5
Avinash Dongre <avi5kdon@gmail.com>
2018-04-21 01:39:38 +0530

    1

--- src/main/java/com/some/Initialize.java
+++ src/main/java/com/some/Initialize.java
@@ -1,11 +1,17 @@
 package com.some;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class MyApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class Initialize extends SpringBootServletInitializer implements WebApplicationInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplicationBuilder.class);
+	}
 
 }
