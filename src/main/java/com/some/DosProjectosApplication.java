e08fd4f448de0c74bfd70032809f46d022903b39
Avinash Dongre <avi5kdon@gmail.com>
2018-04-24 02:16:19 +0530

    DW

---
+++ src/main/java/com/some/DosProjectosApplication.java
@@ -0,0 +1,12 @@
+package com.some;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosProjectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosProjectosApplication.class, args);
+	}
+}
