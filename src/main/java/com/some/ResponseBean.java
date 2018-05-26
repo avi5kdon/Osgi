822ca47c21593fbb0982792a82f1a01f2605cf91
Avinash Dongre <avi5kdon@gmail.com>
2018-05-27 03:00:13 +0530

    Fcc

--- src/main/java/com/some/ResponseBean.java
+++ src/main/java/com/some/ResponseBean.java
@@ -2,11 +2,12 @@ package com.some;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 @Component
-@Scope(scopeName=WebApplicationContext.SCOPE_REQUEST)
+@Scope(scopeName=WebApplicationContext.SCOPE_REQUEST,proxyMode=ScopedProxyMode.TARGET_CLASS)
 public class ResponseBean {
 
 	private String response;
